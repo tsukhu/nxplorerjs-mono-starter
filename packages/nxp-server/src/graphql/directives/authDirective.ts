7666bc5332f077e90a65dd8ad768fc0eb82f3dfe
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-11-03 09:15:34 +0530

    all dependencies upgraded and sorted out Apollo Server includes

--- packages/nxp-server/src/graphql/directives/authDirective.ts
+++ packages/nxp-server/src/graphql/directives/authDirective.ts
@@ -1,5 +1,5 @@
 import { defaultFieldResolver } from 'graphql';
-import { SchemaDirectiveVisitor } from 'graphql-tools';
+import { SchemaDirectiveVisitor } from 'apollo-server';
 import { getUserRole } from '@nxp/nxp-core';
 import { UnAuthorizedError } from '../errors';
 
